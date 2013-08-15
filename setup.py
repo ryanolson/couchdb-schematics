b777f0862a6ab2671846d6c4722bf17845f8fec4
Ryan Olson <rmolson@gmail.com>
2013-08-14 19:30:34 -0600

    removed setuptools requirement

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(
     ],
     tests_require=test_requirements,
     install_requires=[
-        'setuptools>=0.8',
         'schematics>=0.8.0-alpha',
         'CouchDB'
     ] + test_requirements,
