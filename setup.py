078dec83819361d694312b85723725f16e8cefc7
Ryan Olson <rmolson@gmail.com>
2013-10-31 17:13:15 -0600

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,24 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+    setup.py
+    ~~~~~~~~
+
+    :author: Ryan Olson
+    :copyright: (c) 2013 Ryan Olson
+    :license: Apache v2, see LICENSE for more details.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
 
 from setuptools import setup
 
@@ -10,7 +29,7 @@ test_requirements=[
 setup(
     name='couchdb-schematics',
     license='BSD',
-    version='0.2.0-alpha',
+    version='0.2.1-alpha',
     description='',
     author=u'',
     author_email='',
